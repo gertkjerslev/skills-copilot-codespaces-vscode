6375edf675b4be76ed40f2154082af386cfe09a0
Gert Kjerslev <gertk@v220.dk>
2023-10-10 10:17:49 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,34 @@
+// Create web server
+// npm install express --save
+// npm install body-parser --save
+// npm install mongoose --save
+// npm install bcrypt --save
+// npm install jsonwebtoken --save
+// npm install nodemon --save-dev
+// npm install passport --save
+// npm install passport-jwt --save
+// npm install passport-local --save
+// npm install validator --save
+// npm install lodash --save
+// npm install moment --save
+// npm install mongoose-unique-validator --save
+// npm install bcryptjs --save
+// npm install --save-dev jest
+// npm install supertest --save-dev
+// npm install env-cmd --save-dev
+// npm install --save-dev cross-env
+// npm install --save-dev jest-environment-node
+// npm install --save-dev jest-environment-jsdom
+// npm install --save-dev jest-environment-jsdom-sixteen
+// npm install --save-dev jest-watch-typeahead
+// npm install --save-dev jest-serializer-vue
+// npm install --save-dev vue-jest
+// npm install --save-dev babel-jest
+// npm install --save-dev babel-core@^7 babel-jest@^24
+// npm install --save-dev babel-core@^7 babel-jest@^24 @babel/preset-env@^7
+// npm install --save-dev @babel/preset-env
+// npm install --save-dev @babel/plugin-syntax-dynamic-import
+// npm install --save-dev @babel/plugin-transform-runtime
+// npm install --save-dev @babel/runtime
+// npm install --save-dev babel-core@^7 babel-jest@^24 @babel/preset-env@^7
+// npm install --save-dev
